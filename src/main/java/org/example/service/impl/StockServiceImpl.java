73fb237241850ab06a6e1172661a99125b552925
Vamsi K <sunny77katta2002@gmail.com>
2025-01-12 12:19:35 +0530

    refactored and tests.

---
+++ src/main/java/org/example/service/impl/StockServiceImpl.java
@@ -0,0 +1,28 @@
+package org.example.service.impl;
+
+import org.example.dto.StockDTO;
+import org.example.entities.Stocks;
+import org.example.repository.StocksRepository;
+import org.example.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockServiceImpl implements StockService {
+    @Autowired
+    private StocksRepository stocksRepository;
+
+    @Override
+    public StockDTO getStockById(String stockId) {
+        Stocks stock = stocksRepository.findByStockId(stockId);
+        return StockDTO.builder()
+                .stockId(stock.getStockId())
+                .stockName(stock.getStockName())
+                .openPrice(stock.getOpenPrice())
+                .closePrice(stock.getClosePrice())
+                .lowPrice(stock.getLowPrice())
+                .highPrice(stock.getHighPrice())
+                .settlementPrice(stock.getSettlementPrice()).build();
+
+    }
+}
